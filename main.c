198ea6c7a93e1c85619845671b41df5a2d0b8d9f
Uni <jorgeaguilarsalvador161@gmail.com>
2026-04-22 10:54:18 +0200

    semilla en main.c

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include "head.h"
 int main() 
 {
-    int nodos = 10;
-    float probabilidad = 0.1;
-
+    ini_ran(12345);
+    int nodos = 20;
+    float probabilidad = 0.2;
     generarErdosRenyi(nodos, probabilidad);
 
     return 0;
